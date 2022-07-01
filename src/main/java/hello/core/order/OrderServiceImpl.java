70e9e7d1df7397548c3f24aa9a3cbe2ff21290af
sijune <sijune0525@gmail.com>
2022-07-02 07:58:11 +0900

    first commit

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); //member를 넘길지 grade를 넘길지 프로젝트 상황에 따라 다르다.
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
